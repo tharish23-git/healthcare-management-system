6773dd01ff0c43883a4c1b291c4bcfc60213998d
Tharishna <tharishnabalmic@gmail.com>
2026-06-04 09:07:40 +0800

    Pls work cb

--- backend/server.js
+++ backend/server.js
@@ -100,4 +100,4 @@ app.put('/api/slots/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));RT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));`));
